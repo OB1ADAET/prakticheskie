f6ae61ed046c40cc99ec965a8763901b3f4251b6
OB1ADAET <116683120+OB1ADAET@users.noreply.github.com>
2022-10-26 14:09:09 +0300

    Add files via upload

---
+++ prak2.2.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("\ny/x|   1\t2\t3\t4\t5\t6\t7\t8\t9");
+Console.Write("--------------------------------------------------------------------------");
+Console.WriteLine();
+for (int i = 1; i < 10; i++)
+{
+    Console.Write(i + "   |  ");
+    for (int j = 1; j < 10; j++)
+    {
+        Console.Write("\t" + i * j);
+    }
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
+Console.ReadLine();
